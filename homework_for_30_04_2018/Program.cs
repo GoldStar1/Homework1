2e5f5f1b5f730d927d51202da3e7579f20449ce3
Inna <1inna.balabon@gmail.com>
2019-01-12 17:51:30 +0200

    Добавьте файлы проекта.

---
+++ homework_for_30_04_2018/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_for_30_04_2018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Plese input number \"a\":  ");
+            int a;
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Plese input number \"b\":  ");
+            int b;
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("New numbers are: ");
+            Console.Write("A:   ");
+            do
+            {
+                Console.Write(a % 10);
+                a = a / 10;
+            } while (a!=0);
+            Console.WriteLine();
+
+            Console.Write("B:   ");
+            do
+            {
+                Console.Write(b % 10);
+                b = b / 10;
+            } while (b != 0);
+
+
+            Console.ReadKey();
+        }
+    }
+}
